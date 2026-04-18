1d222582931cf4635dc70543a1a91a332888368b
juann-ign <juanignacioatencio1@gmail.com>
2026-04-18 20:53:49 -0300

    feat: register auth router and enable login endpoint in main app

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.v1 import parking
+from app.api.v1 import parking, auth
 from app.core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME)
@@ -13,6 +13,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Aquí incluimos los routers de cada módulo
+app.include_router(auth.router, prefix=settings.API_V1_STR)
 app.include_router(parking.router, prefix=settings.API_V1_STR)
 
 @app.get("/")
